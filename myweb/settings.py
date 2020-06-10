6068038ab3737131d3282b6be22220e105f36fd4
Brian Maverick <toirovshohruxmirzo@gmail.com>
2020-06-10 19:40:25 +0900

    Some change

--- myweb/settings.py
+++ myweb/settings.py
@@ -82,14 +82,20 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DEMO_DB',
-        'USER': 'postgres',
-        'PASSWORD':'Beauty2000$',
-        'HOST': 'localhost',
-        'PORT':5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'DEMO_DB',
+#         'USER': 'postgres',
+#         'PASSWORD':'Beauty2000$',
+#         'HOST': 'localhost',
+#         'PORT':5432
+#     }
+# }
 
 
 
